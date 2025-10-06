5632fea8237d1b724396867505449324a54cdad3
Bruno Arial Ramos <brunoarial@gmail.com>
2025-10-06 16:19:09 -0300

    Versão 1.3.0
    Implementado um novo gráfico (widget trading view) que o usuário vizualiza as cotações em tempo real das criptomoedas

---
+++ graficocripto.js
@@ -0,0 +1,35 @@
+new TradingView.widget(
+{
+    "width": "80%",
+    "height": "90%",
+    "symbol": "BINANCE:BTCBRL",
+    "interval": "D",
+    "timezone": "Etc/UTC",
+    "theme": "dark",
+    "style": "1",
+    "locale": "br",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "allow_symbol_change": true,
+    "container_id": "tradingview_f7a6a",
+
+    "exchanges": [
+        "BINANCE",
+        "MERCADOBTC", 
+        "COINBASE"
+    ],
+
+    "types": [
+        "crypto", 
+        "forex" 
+    ],
+
+    "symbols": [
+        {"proName": "BINANCE:BTCBRL", "title": "Bitcoin / Real"},
+        {"proName": "BINANCE:ETHBRL", "title": "Ethereum / Real"},
+        {"proName": "BINANCE:XRPBRL", "title": "XRP / Real"},
+        {"proName": "BINANCE:SOLBRL", "title": "Solana / Real"},
+        {"proName": "BINANCE:DOGEBRL", "title": "Dogecoin / Real"}
+    ]
+}
+);
